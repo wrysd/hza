1d7663a031790fab586c1eef246fff8404539a41
wrysd <476147537@qq.com>
2018-06-20 10:58:56 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <leftNav></leftNav>
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import leftNav from '@/components/common/leftNav'
 export default {
-  name: 'App'
+  name: 'app',
+  components:{
+    leftNav
+  }
 }
 </script>
 
@@ -16,8 +22,16 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+
+.main{
+  float: left;
+  width: 95%;
+  background-color: #EFF2F7;
+  height: 100%;
+  overflow: auto;
 }
 </style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="pos">
+</div>
+</template>
+
+<script>
+export default {
+  name: "Pos"
+};
+</script>
+<style scoped>
+</style>
